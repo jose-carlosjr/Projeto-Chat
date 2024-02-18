09c2674abec4c8e3e1fe441fbf9dcd1a1de41e5e
Jose <josecarlos099916@gmail.com>
2024-02-18 15:29:36 -0300

    Criando o servidor

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,18 @@
-console.log('Olá, mundo!')
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+// wss RECEBE O SERVIDOR
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
+
+wss.on("connection", (ws) => {
+    ws.on("error", console.error)
+
+    // ENVIA A MENSAGEM PARA TODOS OS CLIENTES
+    ws.on("message", (data) => {
+        wss.clients.forEach((client) => {client.send(data.toString())})
+    })
+
+    console.log("Client connected")
+})
